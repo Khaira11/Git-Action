e2fc42218d0c4e5154aad957bd851126565db4aa
khaira23 <harpreetkhaira@hotmail.com>
2026-06-02 11:55:40 +0530

    first project

---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "public_ec2_ip"  { value = aws_instance.public.public_ip }
+output "private_ec2_ip" { value = aws_instance.private.private_ip }
+output "vpc_id"         { value = aws_vpc.main.id }
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+  default = "ap-south-1"  # Mumbai — closest to India
+}
+
+variable "project_name" {
+  default = "my-gitops-project"
+}
+
+variable "ami_id" {
+  # Amazon Linux 2023 in ap-south-1
+  default = "ami-0f58b397bc5c1f2e8"
+}
